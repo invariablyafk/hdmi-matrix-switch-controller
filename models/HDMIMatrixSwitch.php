5d9bcbc323ca45a54771bb1f912d1480038e2da6
Tal <tgraff@talg.us>
2018-12-20 11:29:09 -0700

    updates for current functions

--- models/HDMIMatrixSwitch.php
+++ models/HDMIMatrixSwitch.php
@@ -114,71 +114,71 @@ class HDMIMatrixSwitch {
 		switch(strtolower($command)){
 
 			case "a1";
-				exec("./command-line-tools/hdmi_matrix2 1 1"); // TV a SELECT inputl
+				exec("./command-line-tools/hdmi-matrix 1 1", $output); // TV a SELECT inputl
 				break;
 			case "a2";
-				exec("./command-line-tools/hdmi_matrix2 1 2"); // TV a SELECT input2
+				exec("./command-line-tools/hdmi-matrix 1 2", $output); // TV a SELECT input2
 				break;
 			case "a3";
-				exec("./command-line-tools/hdmi_matrix2 1 3"); // TV a SELECT input3
+				exec("./command-line-tools/hdmi-matrix 1 3", $output); // TV a SELECT input3
 				break;
 			case "a4";
-				exec("./command-line-tools/hdmi_matrix2 1 4"); // TV a SELECT input4
+				exec("./command-line-tools/hdmi-matrix 1 4", $output); // TV a SELECT input4
 				break;
 
 
 			case "b1";
-				exec("./command-line-tools/hdmi_matrix2 2 1"); // TV b SELECT inputl
+				exec("./command-line-tools/hdmi-matrix 2 1", $output); // TV b SELECT inputl
 				break;
 			case "b2";
-				exec("./command-line-tools/hdmi_matrix2 2 2"); // TV b SELECT input2
+				exec("./command-line-tools/hdmi-matrix 2 2", $output); // TV b SELECT input2
 				break;
 			case "b3";
-				exec("./command-line-tools/hdmi_matrix2 2 3"); // TV b SELECT input3
+				exec("./command-line-tools/hdmi-matrix 2 3", $output); // TV b SELECT input3
 				break;
 			case "b4";
-				exec("./command-line-tools/hdmi_matrix2 2 4"); // TV b SELECT input4
+				exec("./command-line-tools/hdmi-matrix 2 4", $output); // TV b SELECT input4
 				break;
 
 
 			case "c1";
-				exec("./command-line-tools/hdmi_matrix2 3 1"); // TV c SELECT inputl
+				exec("./command-line-tools/hdmi-matrix 3 1", $output); // TV c SELECT inputl
 				break;
 			case "c2";
-				exec("./command-line-tools/hdmi_matrix2 3 2"); // TV c SELECT input2
+				exec("./command-line-tools/hdmi-matrix 3 2", $output); // TV c SELECT input2
 				break;
 			case "c3";
-				exec("./command-line-tools/hdmi_matrix2 3 3"); // TV c SELECT input3
+				exec("./command-line-tools/hdmi-matrix 3 3", $output); // TV c SELECT input3
 				break;
 			case "c4";
-				exec("./command-line-tools/hdmi_matrix2 3 4"); // TV c SELECT input4
+				exec("./command-line-tools/hdmi-matrix 3 4", $output); // TV c SELECT input4
 				break;
 
 
 			case "d1";
-				exec("./command-line-tools/hdmi_matrix2 4 1"); // TV d SELECT inputl
+				exec("./command-line-tools/hdmi-matrix 4 1", $output); // TV d SELECT inputl
 				break;
 			case "d2";
-				exec("./command-line-tools/hdmi_matrix2 4 2"); // TV d SELECT input2
+				exec("./command-line-tools/hdmi-matrix 4 2", $output); // TV d SELECT input2
 				break;
 			case "d3";
-				exec("./command-line-tools/hdmi_matrix2 4 3"); // TV d SELECT input3
+				exec("./command-line-tools/hdmi-matrix 4 3", $output); // TV d SELECT input3
 				break;
 			case "d4";
-				exec("./command-line-tools/hdmi_matrix2 4 4"); // TV d SELECT input4
+				exec("./command-line-tools/hdmi-matrix 4 4", $output); // TV d SELECT input4
 				break;
 
 			case "all1";
-				exec("./command-line-tools/hdmi_matrix2 1"); // TV ALL SELECT inputl
+				exec("./command-line-tools/hdmi-matrix 1", $output); // TV ALL SELECT inputl
 				break;
 			case "all2";
-				exec("./command-line-tools/hdmi_matrix2 2"); // TV ALL SELECT input2
+				exec("./command-line-tools/hdmi-matrix 2", $output); // TV ALL SELECT input2
 				break;
 			case "all3";
-				exec("./command-line-tools/hdmi_matrix2 3"); // TV ALL SELECT input3
+				exec("./command-line-tools/hdmi-matrix 3", $output); // TV ALL SELECT input3
 				break;
 			case "all4";
-				exec("./command-line-tools/hdmi_matrix2 4"); // TV ALL SELECT input4
+				exec("./command-line-tools/hdmi-matrix 4", $output); // TV ALL SELECT input4
 				break;
 
 
@@ -187,7 +187,7 @@ class HDMIMatrixSwitch {
 				break;
 
 		}
-
+print_r($output);
 		/*
 		if(isset($commandBytes)){
 			foreach($commandBytes as $byte){
